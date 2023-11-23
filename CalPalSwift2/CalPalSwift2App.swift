35f1b4ab65856cedf0d13e634bb73eb4a3be1656
Jan Phillip Schwietzer <schwietzer.jan@gmail.com>
2023-11-23 12:46:35 +0100

    Initial Commit

---
+++ CalPalSwift2/CalPalSwift2App.swift
@@ -0,0 +1,17 @@
+//
+//  CalPalSwift2App.swift
+//  CalPalSwift2
+//
+//  Created by Jan Schwietzer on 23.11.23.
+//
+
+import SwiftUI
+
+@main
+struct CalPalSwift2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
